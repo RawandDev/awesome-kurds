106ddc037bd0c9c3ef6dba78434c1e6850cbf1bf
RawandDev <82733587+RawandDev@users.noreply.github.com>
2021-12-25 21:25:27 +0000

    Deploying to gh-pages from @ RawandDev/awesome-kurds@d1eb6460ccb998727cff6c915471647ccf9a647d 🚀

---
+++ _next/static/VB9OUoO-7wJ2-B0uMaURT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/670-681cab7e20a1771c.js","static/css/ad47a3cdab8a3ae2.css","static/chunks/pages/index-1b8638eccf98aef4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
